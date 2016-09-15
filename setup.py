1308e28d9bb53555a62693bb4f8b0c69f23c3fe7
Man Long Wong <manlong89@gmail.com>
2016-09-15 23:10:15 +0000

    setup.py edited online with Bitbucket

--- setup.py
+++ setup.py
@@ -5,19 +5,19 @@ import sys
 
 # Python version
 if sys.version_info[:2] < (2, 7):
-    print('PyFR requires Python 2.7 or newer')
+    print('UFloAT requires Python 2.7 or newer')
     sys.exit(-1)
 
 version = '1.0'
 
 # Modules.
-modules = ['fpalpost.derivatives',
-           'fpalpost.readers']
+modules = ['ufloat.derivatives',
+           'ufloat.readers']
 
-setup(name='FPAL-Post',
+setup(name='UFloAT',
       version=version,
-      description='Postprocessing utilities for FPAL\'s code',
-      author='Flow Physics and Aeroacoustics Laboratory of Stanford University',
+      description='Postprocessing utilities for codes in UFPA lab',
+      author='Unsteady Flow Physics and Aeroacoustics Laboratory of Stanford University',
       author_email='wongml@stanford.edu',
-      packages=['fpalpost'] + modules,
+      packages=['ufloat'] + modules,
       )
