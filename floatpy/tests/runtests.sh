f6a5e4378f802b97c69a55689148ac87c5a1380e
Akshay Subramaniam <akshaysubr@gmail.com>
2017-07-26 15:19:54 -0700

    Added .travis.yml file for Travis CI

--- floatpy/tests/runtests.sh
+++ floatpy/tests/runtests.sh
@@ -6,13 +6,16 @@ else
     MPIEXEC="${MPIEXEC}"
 fi
 
-for testfile in test_*.py; do
+# Get the directory of this file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for testfile in ${DIR}/test_*.py; do
     python ${testfile}
     if [ $? -ne 0 ]; then
         exit 1
     fi
 done
 
-for testfile in mpitest_*.py; do
+for testfile in ${DIR}/mpitest_*.py; do
     ${MPIEXEC} -n 4 python ${testfile}
 done
